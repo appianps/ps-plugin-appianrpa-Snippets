a6cd5ee7e0c57d469dcd445bfb47b1a625e03236
Javier Advani <javier.advani@appian.com>
2020-05-07 15:02:59 +0200

    browser manager first version

---
+++ snippets-examples/browser-manager/src/main/java/com/appian/rpa/robot/browser_manager/App.java
@@ -0,0 +1,13 @@
+package com.appian.rpa.robot.browser_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
