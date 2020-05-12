7c19ee2fffc0f2f12e299f091f2f259457de69ff
Irene Monge <64584804+irene-ms@users.noreply.github.com>
2020-05-12 17:19:46 +0200

    PS-430: Added method getWindowTitleRegex

--- snippets-libraries/snippet-3270-commons/src/main/java/com/appian/rpa/snippet/clients/SGCCommonsExtended.java
+++ snippets-libraries/snippet-3270-commons/src/main/java/com/appian/rpa/snippet/clients/SGCCommonsExtended.java
@@ -5,84 +5,83 @@ import com.appian.rpa.snippet.IBM3270Commons;
 import com.novayre.jidoka.client.api.IRobot;
 import com.novayre.jidoka.client.api.multios.IClient;
 
-
 /**
  * IBM3270Commons extension for SGC client
  */
 public class SGCCommonsExtended extends IBM3270Commons {
-	
+
 	/**
 	 * The Constant TITLE_REGEX.
 	 */
 	public static final String IBM3270_LOGIN_TITLE_REGEX = "(?i)SGC verde - wc3270";
-	
+
 	/**
 	 * Line separator
 	 */
 	private static final String LINE_SEPARATOR = "\n";
 
-	
 	/**
 	 * Default X-coordinate
 	 */
 	private static final int MAX_COORD_X = 80;
-	
+
 	/**
 	 * Default Y-coordinate
 	 */
 	private static final int MAX_COORD_Y = 24;
-	
-	
-	
+
 	/**
 	 * Default constructor
+	 * 
 	 * @param server
 	 * @param client
 	 * @param robot
 	 */
 	public SGCCommonsExtended(IClient client, IRobot robot) {
-		
+
 		super(client, robot);
 
 		setMaxCoordX(MAX_COORD_X);
 		setMaxCoordY(MAX_COORD_Y);
 	}
 
-	
 	/**
 	 * Select all text in the screen
 	 */
+	@Override
 	public void selectAllText() {
-		
+
 		moveToBottonRightCorner();
-		
-		keyboard.alt(" ").pause(ConstantsWaits.PAUSE_MENU_COPY).type("e").pause(ConstantsWaits.PAUSE_MENU_COPY).type("s").pause();
-		keyboard.alt(" ").pause(ConstantsWaits.PAUSE_MENU_COPY).type("e").pause(ConstantsWaits.PAUSE_MENU_COPY).type("o").pause();
-		
+
+		keyboard.alt(" ").pause(ConstantsWaits.PAUSE_MENU_COPY).type("e").pause(ConstantsWaits.PAUSE_MENU_COPY)
+				.type("s").pause();
+		keyboard.alt(" ").pause(ConstantsWaits.PAUSE_MENU_COPY).type("e").pause(ConstantsWaits.PAUSE_MENU_COPY)
+				.type("o").pause();
+
 	}
-	
-	
+
 	/**
 	 * Activate a window by title
 	 */
+	@Override
 	public void activateWindow() {
-		
+
 		client.activateWindow(IBM3270_LOGIN_TITLE_REGEX);
-		
+
 		client.pause();
 	}
-	
+
 	/**
 	 * Move the cursor to the bottom right corner of the screen
 	 */
 	@Override
 	public void moveToBottonRightCorner() {
-		
-		keyboard.down().pause(); 
+
+		keyboard.down().pause();
 		keyboard.control("g").pause().control("g").pause();
-		
+
 	}
-	
+
 	/**
 	 * Split the lines of text on the screen
 	 */
@@ -91,4 +90,13 @@ public class SGCCommonsExtended extends IBM3270Commons {
 		return screen.split(LINE_SEPARATOR);
 	}
 
+	/**
+	 * Returns the window title regex
+	 * 
+	 * @return
+	 */
+	@Override
+	public String getWindowTitleRegex() {
+		return IBM3270_LOGIN_TITLE_REGEX;
+	}
 }
