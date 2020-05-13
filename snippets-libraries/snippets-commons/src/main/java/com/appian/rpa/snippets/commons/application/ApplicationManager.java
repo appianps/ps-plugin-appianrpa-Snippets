4fa100b2114889714467aff30aff58c14489f79f
Victor Diaz <victor.diaz@appian.com>
2020-05-13 09:45:58 +0200

    PS-467.  robot-application-manager ready to deploy
    Robot application manager ready to deploy, changes ro close, add getWindow, javadoc, manageexception,..

--- snippets-libraries/snippets-commons/src/main/java/com/appian/rpa/snippets/commons/application/ApplicationManager.java
+++ snippets-libraries/snippets-commons/src/main/java/com/appian/rpa/snippets/commons/application/ApplicationManager.java
@@ -1,32 +1,28 @@
 package com.appian.rpa.snippets.commons.application;
 
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
-import com.novayre.jidoka.client.api.IJidokaServer;
 import com.novayre.jidoka.client.api.IRobot;
 import com.novayre.jidoka.client.api.IWaitFor;
-import com.novayre.jidoka.client.api.JidokaFactory;
 import com.novayre.jidoka.client.api.exceptions.JidokaFatalException;
-import com.novayre.jidoka.client.api.multios.EClientShowWindowType;
-import com.novayre.jidoka.client.api.multios.IClient;
+import com.novayre.jidoka.windows.api.IWindows;
 
 import mmarquee.automation.UIAutomation;
 import mmarquee.automation.controls.Application;
+import mmarquee.automation.controls.Window;
 
 /**
- * Class to manage the actions refered to a desktop application
+ * Class to manage the actions referred to a desktop application
  *
  */
 public class ApplicationManager {
 
 	/** Client module */
-	private IClient client;
+	private IWindows client;
 
 	/** IWaitFor instance */
 	private IWaitFor waitFor;
-
-	/** IJidokaServer instance */
-	private IJidokaServer<?> server;
 
 	/** UIAutomation module */
 	private UIAutomation automation;
@@ -38,30 +34,33 @@ public class ApplicationManager {
 	private String appDir;
 
 	/** Application window title regex */
-	private String windowTittle;
+	private String windowTittleRegex;
 
 	/** Application */
 	private Application application;
 
+	/** Application window */
+	private Window window;
+
 	/**
 	 * ApplicationManager constructor
 	 * 
-	 * @param robot        IRobot instance
-	 * @param appLauncher  App launcher name
-	 * @param appDir       App launcher directory
-	 * @param windowTittle App window title regex
+	 * @param robot             IRobot instance (i.i. this)
+	 * @param appLauncher       App launcher name (i.e. "calc.exe")
+	 * @param appDir            App launcher directory (i.e.
+	 *                          "C:\\Windows\\system32\\")
+	 * @param windowTittleRegex App window title regex (i.e. ".*Calculator.*")
 	 * 
 	 */
-	public ApplicationManager(IRobot robot, String appLauncher, String appDir, String windowTittle) {
+	public ApplicationManager(IRobot robot, String appLauncher, String appDir, String windowTittleRegex) {
 
-		this.server = JidokaFactory.getServer();
-		this.client = IClient.getInstance(robot);
+		this.client = IWindows.getInstance(robot);
 		this.waitFor = client.waitFor(robot);
 		this.automation = UIAutomation.getInstance();
 
 		this.appLauncher = appLauncher;
 		this.appDir = appDir;
-		this.windowTittle = windowTittle;
+		this.windowTittleRegex = windowTittleRegex;
 
 	}
 
@@ -78,11 +77,11 @@ public class ApplicationManager {
 			application = automation.launchOrAttach(execPath);
 			application.waitForInputIdle(30);
 
-			// wait app opened
-			waitFor.window(this.windowTittle);
+			// çWait for the window to open
+			waitFor.window(this.windowTittleRegex);
 
-			// Send a screenshot to the console
-			server.sendScreen("Applicacion started");
+			window = automation.getDesktopWindow(Pattern.compile(windowTittleRegex));
+
 		} catch (Exception e) {
 			throw new JidokaFatalException(
 					"An error appeared while attempting to open the application " + e.getMessage(), e);
@@ -90,13 +89,30 @@ public class ApplicationManager {
 	}
 
 	/**
-	 * Close the application
+	 * Close the application with a backup way in case the first one doesn't work
 	 */
 	public void closeApp() throws JidokaFatalException {
 
 		try {
-			this.activateWindow();
-			client.typeText(client.keyboardSequence().typeAltF(4));
+			if (window != null) {
+				window.close();
+				window = null;
+				boolean closed = client.waitCondition(15, 1000, "Closing window " + windowTittleRegex, null, false,
+						false, (i, T) -> {
+							return client.getWindow(windowTittleRegex) == null;
+						});
+				if (!closed && application != null) {
+					application.close(Pattern.compile(windowTittleRegex));
+
+					closed = client.waitCondition(15, 1000, "Closing window " + windowTittleRegex, null, false, false,
+							(i, T) -> {
+								return client.getWindow(windowTittleRegex) == null;
+							});
+					if (!closed) {
+						throw new JidokaFatalException("Can't close the window " + windowTittleRegex);
+					}
+				}
+			}
 		} catch (Exception e) {
 			throw new JidokaFatalException("Error while closing the application", e);
 		}
@@ -108,11 +124,9 @@ public class ApplicationManager {
 	public void activateWindow() throws JidokaFatalException {
 		try {
 			// Activate the Application
-			client.activateWindow(this.windowTittle);
-			client.showWindow(client.getWindow(this.windowTittle).getId(), EClientShowWindowType.SHOW);
-
+			window.focus();
 			// Wait to the window to activate
-			waitFor.windowActive(this.windowTittle);
+			waitFor.windowActive(this.windowTittleRegex);
 		} catch (Exception e) {
 			throw new JidokaFatalException(
 					"An unexpected error appeared while attempting to activate the application window as foreground element",
@@ -121,11 +135,23 @@ public class ApplicationManager {
 	}
 
 	/**
+	 * The Application object is retrieved through this method so the robot has easy
+	 * access to all its functionalities.
 	 * 
-	 * @return application
+	 * @return application object
 	 */
 	public Application getApplication() {
 		return application;
+	}
+
+	/**
+	 * The Application object is retrieved through this method so the robot has easy
+	 * access to all its functionalities.
+	 * 
+	 * @return window object
+	 */
+	public Window getWindow() {
+		return window;
 	}
 
 }
