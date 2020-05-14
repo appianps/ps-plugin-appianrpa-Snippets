86494c42c5cbe399790165df1c292dafd4545dee
Irene Monge <64584804+irene-ms@users.noreply.github.com>
2020-05-14 11:10:21 +0200

    PS-430: The class is renamed. Stop extending to the IBM3270Commons class.

---
+++ snippets-libraries/snippet-3270-commons/src/main/java/com/appian/rpa/snippet/page/IBM3270Page.java
@@ -0,0 +1,81 @@
+package com.appian.rpa.snippet.page;
+
+import com.appian.rpa.snippet.ConstantsWaits;
+import com.appian.rpa.snippet.IBM3270Commons;
+import com.novayre.jidoka.client.api.exceptions.JidokaException;
+
+/**
+ * Class to manage Remote Pages
+ */
+public abstract class IBM3270Page {
+
+	/**
+	 * IBM3270Commons instance
+	 */
+	private IBM3270Commons commons;
+
+	/**
+	 * Class constructor
+	 * 
+	 * @param commons
+	 * @throws JidokaException
+	 */
+	public IBM3270Page(IBM3270Commons commons) throws JidokaException {
+		this.commons = commons;
+		assertIsThisPage();
+	}
+
+	/**
+	 * Unique text on each page for recognition
+	 * 
+	 * @return A unique text on each page for recognition
+	 */
+	public abstract String getUnivocalRegex();
+
+	/**
+	 * Check that the robot is on the correct page (current page)
+	 * 
+	 * @throws JidokaGenericException
+	 */
+	public Boolean assertIsThisPage() throws JidokaException {
+
+		String univocalRegex = getUnivocalRegex();
+
+		try {
+			if (univocalRegex != null) {
+				commons.locateText(ConstantsWaits.DEFAULT_NUMBER_OF_RETRIES_LOCATING_TEXT, getUnivocalRegex());
+			}
+		} catch (Exception e) {
+
+			throw new JidokaException(getPageName());
+		}
+
+		return true;
+	}
+
+	/**
+	 * Indicates the name of the current page (class)
+	 * 
+	 * @return
+	 */
+	public String getPageName() {
+		return this.getClass().getSimpleName();
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public IBM3270Commons getCommons() {
+		return commons;
+	}
+
+	/**
+	 * 
+	 * @param commons
+	 */
+	public void setCommons(IBM3270Commons commons) {
+		this.commons = commons;
+	}
+
+}
